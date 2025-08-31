9e3fcf3bcd9685e6ce5ed7f559a771e9bf723b67
Jinesh Van <vjeenesh@gmail.com>
2025-08-31 17:15:22 +0530

    Decimal to binary conversion

---
+++ dec-to-bin.py
@@ -0,0 +1,23 @@
+def DecimalToBinary(decNum):
+    
+    pow = 1
+    bin = 0
+
+    while (decNum > 0):
+        rem = decNum % 2
+        decNum //= 2
+        bin += (rem * pow)
+        pow *= 10
+    
+    return bin
+
+
+for i in range(0, 11):
+    print(f"{i} in binary is {DecimalToBinary(i)}")
+
+# print(DecimalToBinary(10))
+# print(DecimalToBinary(42))
+# print(DecimalToBinary(2))
+# print(DecimalToBinary(69))
+# print(DecimalToBinary(246))
+# print(DecimalToBinary(1225))
