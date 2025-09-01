65da6820a5bea8aa974e84e3e035d5b019c25332
Jinesh Van <vjeenesh@gmail.com>
2025-09-02 00:46:50 +0530

    bitwise operations

--- dec-to-bin.py
+++ dec-to-bin.py
@@ -20,8 +20,8 @@ print(4 & 8) # Bitwise AND
 print(4 | 8) # Bitwise OR
 print(4 ^ 8) # Bitwise XOR
 print(~4)    # Bitwise NOT
-print(4 << 1) # Left Shift
-print(4 >> 1) # Right Shift
+print(4 << 1) # Left Shift --> a >> b means a * (2^b)
+print(4 >> 1) # Right Shift --> a << b means a / (2^b)
 
 # print(DecimalToBinary(10))
 # print(DecimalToBinary(42))
